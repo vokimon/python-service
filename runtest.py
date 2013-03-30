358e4376f3464ffc33143fde47b97354e5d6715b
David García Garzón <voki@canvoki.net>
2013-03-30 10:28:31 +0100

    Added runtest.py and renamed tests files

---
+++ runtest.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+import unittest
+from Service_test import *
+#from SvnSandbox_test import * 
+#from GitSandbox_test import *
+from deansi_test import *
+from sign import *
+
+unittest.main()
+
